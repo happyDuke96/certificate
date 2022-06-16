786b80106867f18db5978111ebb8c61048f3c58d
abbos abduhakimov <abbos9113@mail.com>
2022-06-16 14:09:27 +0500

    Initial commit

---
+++ src/main/java/uz/abbos/certificate/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package uz.isystem.certificateservicesecond.exception;
+
+
+public class BadRequestException extends RuntimeException{
+    public BadRequestException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/uz/abbos/certificate/service/DirectionService.java
@@ -0,0 +1,33 @@
+package uz.isystem.certificateservicesecond.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import uz.isystem.certificateservicesecond.exception.BadRequestException;
+import uz.isystem.certificateservicesecond.model.Direction;
+import uz.isystem.certificateservicesecond.repository.DirectionRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class DirectionService {
+    private DirectionRepository directionRepository;
+
+    public boolean create(Direction direction) {
+        directionRepository.save(direction);
+        return true;
+    }
+
+    public List<Direction> getAll() {
+        return directionRepository.findAll();
+    }
+
+    public Direction getById(Integer id){
+        Optional<Direction> optional = directionRepository.findById(id);
+        if(optional.isEmpty()){
+            throw new BadRequestException("Direction not found");
+        }
+        return optional.get();
+    }
+}
